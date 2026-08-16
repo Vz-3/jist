c1193aa4371c8f0f3066bd130d6aa6453051157d
Vz-3 <vondc6201@gmail.com>
2026-08-17 00:06:44 +0800

    fix: company attr as query is company query is invalid for company section

--- scripts/indeed.js
+++ scripts/indeed.js
@@ -1,18 +1,18 @@
 function extractDetails() {
     const params = new URLSearchParams(window.location.search);
     const q = (id) => { return document.querySelector(id)?.textContent?.trim() ?? null };
+    const [date, site, url] = [new Date().toISOString().split('T')[0], window.location.hostname, window.location.href]
 
-    // company
     if (window.location.pathname.includes('jobs') && (params.get('jk'))) {
         let [company, job_location] = q('[data-testid="jobDetailSubtitle"]').split(' - ');
 
         return {
             "job_title": q('[data-testid="jobsearch-JobInfoHeader-title"]'),
-            "company": q('[data-testid="inlineHeader-companyName"]'),
+            "company": company,
             "job_location": job_location,
-            "date": new Date().toISOString().split('T')[0],
-            "site": window.location.hostname,
-            "url": window.location.href,
+            "date": date,
+            "site": site,
+            "url": url,
         }
     } else if (params.get('vjk')) {
         // default
@@ -20,9 +20,9 @@ function extractDetails() {
             "job_title": document.querySelector('[data-testid="jobsearch-JobInfoHeader-title"]')?.querySelector('span')?.textContent,
             "company": q('[data-testid="inlineHeader-companyName"]'),
             "job_location": q('[data-testid="inlineHeader-companyLocation"]'),
-            "date": new Date().toISOString().split('T')[0],
-            "site": window.location.hostname,
-            "url": window.location.href,
+            "date": date,
+            "site": site,
+            "url": url,
         }
     }
     return null;
